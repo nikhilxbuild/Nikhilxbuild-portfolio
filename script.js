c8ddcf1a0413c750730b581c3ff378cb9ad90a4e
Nikhil Sinha <nikhil.sinha380@gmail.com>
2026-02-03 00:59:04 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".icon, .btn, .skill, .card").forEach(el => {
-  el.addEventListener("touchstart", () => {
-    el.classList.add("active");
-  });
-
-  el.addEventListener("touchend", () => {
-    el.classList.remove("active");
-  });
-});
